0fc2d991daf6f81216ea8c8e49137fd377e03655
Wooksang Ryu <today8934@gmail.com>
2022-08-28 12:25:21 +0900

    Stream

---
+++ src/main/java/me/wooksang/java8to11/Stream/App.java
@@ -0,0 +1,58 @@
+package me.wooksang.java8to11.Stream;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args) {
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        List<OnlineClass> javaClasses = new ArrayList<>();
+        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
+        javaClasses.add(new OnlineClass(8, "The Java, 8 to 11", false));
+
+        List<List<OnlineClass>> wooksangEvents = new ArrayList<>();
+        wooksangEvents.add(springClasses);
+        wooksangEvents.add(javaClasses);
+
+        List<String> startsWithSpring =
+                springClasses.stream()
+                        .filter(springClass -> springClass.getTitle().startsWith("spring"))
+                        .map(springClass -> springClass.getTitle())
+                        .collect(Collectors.toList());
+
+        startsWithSpring.forEach(System.out::println);
+
+        List<String> notClosedClass =
+                springClasses.stream()
+                        .filter(springClass -> !springClass.isClosed())
+                        //.filter(Predicate.not(OnlineClass::isClosed))
+                        .map(springClass -> springClass.getTitle())
+                        .collect(Collectors.toList());
+
+        notClosedClass.forEach(System.out::println);
+
+        wooksangEvents.stream()
+                .flatMap(Collection::stream)
+                .forEach(onlineClass -> System.out.println(onlineClass.getId()));
+
+        Stream.iterate(10, i -> i + 1)
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println);
+
+        boolean test = javaClasses.stream()
+                .anyMatch(onlineClass -> onlineClass.getTitle().contains("Test"));
+        System.out.println(test);
+    }
+}
--- src/main/java/me/wooksang/java8to11/defaultmethodandstaticmethod/App.java
+++ src/main/java/me/wooksang/java8to11/defaultmethodandstaticmethod/App.java
@@ -3,31 +3,43 @@ package me.wooksang.java8to11.defaultmethodandstaticmethod;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Spliterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
-        List<String> name = new ArrayList<>();
-        name.add("wooksang");
-        name.add("today8934");
-        name.add("toby");
-        name.add("foo");
+        List<String> names = new ArrayList<>();
+        names.add("wooksang");
+        names.add("today8934");
+        names.add("toby");
+        names.add("foo");
 
-        name.forEach(s -> {
+        names.forEach(s -> {
             System.out.println(s);
         });
 
-        name.forEach(System.out::println);
+        names.forEach(System.out::println);
 
-        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator = names.spliterator();
         while (spliterator.tryAdvance(System.out::println));
 
-        name.stream()
+        names.stream()
                 .map(String::toUpperCase)
                 .filter(s -> s.startsWith("T"))
                 .count();
 
-        name.removeIf(s -> s.startsWith("T"));
+        names.removeIf(s -> s.startsWith("T"));
 
-        name.sort(String::compareToIgnoreCase);
+        names.sort(String::compareToIgnoreCase);
+
+        Stream<String> stringStream = names.stream().map(String::toUpperCase);
+
+        names.stream().map(s -> {
+            System.out.println(s);
+            return s.toUpperCase();
+        });
+
+        List<String> collect = names.parallelStream().map(String::toUpperCase).collect(Collectors.toList());
+
     }
 }
